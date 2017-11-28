874827e254b587ca218cc318947a72dd58bb4808
yate <yateup@163.com>
2017-11-28 18:14:29 +0800

    add login page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="wrap">
-    <Card class="card">
-      <Avatar class="avatar" icon="person" size="large" />
-      <Form class="login">
-        <FormItem prop="user">
-          <Input type="text" placeholder="账号">
-            <Icon type="person" slot="prepend"></Icon>
-          </Input>
-        </FormItem>
-        <FormItem prop="password">
-          <Input type="password" placeholder="密码">
-            <Icon type="locked" slot="prepend"></Icon>
-          </Input>
-        </FormItem>
-        <FormItem>
-          <Button type="primary" long>登录</Button>
-        </FormItem>
-      </Form>
-      <div class="login-tip">直接输入账号和密码即可</div>
-    </Card>
+    <h1>首页</h1>
     <!-- 路由出口,路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
   </div>
@@ -33,25 +15,8 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .wrap{
-  height: 100vh;
-  background-color: #f8f8f9;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.card{
-  width: 360px;
-  padding: 15px 30px;
-  box-shadow: 0 30px 30px -15px #ccc;
-}
-.avatar{
-  margin-bottom: 20px;
-}
-.login-tip{
-  font-size: 10px;
-  text-align: center;
-  color: #c3c3c3;
+
 }
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="wrap">
+    <Card class="card" :class="{active: status}">
+      <Avatar class="login-avatar" icon="person" size="large" />
+      <Form class="login">
+        <FormItem prop="user">
+          <Input type="text" placeholder="账号">
+            <Icon type="person" slot="prepend" size="16"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem prop="password">
+          <Input type="password" placeholder="密码">
+            <Icon type="locked" slot="prepend" size="14"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary" long>登录</Button>
+        </FormItem>
+      </Form>
+      <div class="card-tip">
+        <!-- <Alert type="error" show-icon>错误次数超过3次&emsp;<router-link to="/registe">注册</router-link></Alert> -->
+      </div>
+    </Card>
+    <Card class="card" :class="{active: !status}">
+      <Avatar class="registe-avatar" icon="person" size="large" />
+      <Form class="registe">
+        <FormItem prop="user">
+          <Input type="text" placeholder="账号">
+            <Icon type="person" slot="prepend" size="16"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem prop="password">
+          <Input type="password" placeholder="密码">
+            <Icon type="locked" slot="prepend" size="14"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem prop="password">
+          <Input type="password" placeholder="确认密码">
+            <Icon type="locked" slot="prepend" size="14"></Icon>
+          </Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary" long>注册</Button>
+        </FormItem>
+      </Form>
+      <div class="card-tip"></div>
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  props: ["status"],
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style scoped>
+.wrap{
+  height: 100vh;
+  background: #f8f8f9 url("../assets/bg.jpg") no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card{
+  display: none;
+  background-color: rgba(250,250,250,.93);
+  width: 360px;
+  text-align: center;
+  padding: 15px 30px;
+}
+.card.active{
+  display: block;
+}
+.card .login-avatar{
+  background-color: #87d068;
+  margin-bottom: 20px;
+}
+.card .registe-avatar{
+  margin-bottom: 12px;
+}
+.card-tip{
+  font-size: 10px;
+  text-align: center;
+  color: #c3c3c3;
+}
+.registe .ivu-form-item{
+  margin-bottom: 12px;
+}
+</style>
